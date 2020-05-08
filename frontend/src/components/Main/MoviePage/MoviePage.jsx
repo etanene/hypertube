5725540749198d506b731ffbe1fc826879d4b19b
Maksix <armageddon4839@gmail.com>
2020-05-08 14:40:25 +0300

    added scrolling to top when picking one of recommended movies. Fixed genre list css

--- frontend/src/components/Main/MoviePage/MoviePage.jsx
+++ frontend/src/components/Main/MoviePage/MoviePage.jsx
@@ -1,5 +1,6 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { cn } from '@bem-react/classname';
+import { animateScroll as scroll } from 'react-scroll';
 import MovieInfo from './MovieInfo/MovieInfo';
 import useGetMovieInfo from '../../../services/useGetMovieInfo';
 import useGetMovieSuggestions from '../../../services/useGetMovieSuggestions';
@@ -11,6 +12,10 @@ const MoviePage = ({ match }) => {
   const { movies } = useContext(MovieSearchContext);
   const { imdbId, ytsId } = match.params;
   const movieTorrent = movies.filter((movie) => movie.imdb_code === imdbId)[0];
+  const moviePageCss = cn('MoviePage');
+  useEffect(() => {
+    scroll.scrollToTop();
+  }, [imdbId]);
   const {
     error,
     movieInfo,
@@ -19,13 +24,12 @@ const MoviePage = ({ match }) => {
     errorSuggestions,
     movieSuggestions,
   } = useGetMovieSuggestions(ytsId);
-  const moviePageCss = cn('MoviePage');
   return (
     <div>
       {errorSuggestions && <div>Error suggestions</div>}
       {error && <div>{error}</div>}
       {movieInfo && <MovieInfo cls={moviePageCss} info={movieInfo} movieTorrent={movieTorrent} />}
-      {movieSuggestions && <MovieSuggestions movies={movieSuggestions} />}
+      {movieSuggestions.length > 0 && <MovieSuggestions movies={movieSuggestions} />}
       {movieInfo && <MovieComments title={movieInfo.Title} />}
     </div>
   );
