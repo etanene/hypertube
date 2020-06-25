e5230a35663b5c1b73033312dd5462221210c85b
Maksix <armageddon4839@gmail.com>
2020-06-25 12:47:34 +0300

    added reply button

--- frontend/src/components/Main/MoviePage/MovieComments/CommentList/Comment/Comment.jsx
+++ frontend/src/components/Main/MoviePage/MovieComments/CommentList/Comment/Comment.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { cn } from '@bem-react/classname';
 import { useTranslation } from 'react-i18next';
 import './Comment.css';
@@ -8,6 +8,7 @@ const Comment = ({ comment }) => {
   const commentCss = cn('Comment');
   const { i18n } = useTranslation();
   const date = moment.unix(comment.time).locale(i18n.language).format('D MMMM YYYY HH:mm');
+  const [hiddenReplyField, setHiddenReplyField] = useState(true);
   return (
     <div className={commentCss()}>
       <div className={commentCss('Header')}>
@@ -17,9 +18,28 @@ const Comment = ({ comment }) => {
           <span className={commentCss('Timestamp')}>{date}</span>
         </div>
       </div>
+      <div>test</div>
       <div className={commentCss('Block')}>
         <span className={commentCss('Text')}>{comment.comment}</span>
       </div>
+      <button onClick={() => {
+        setHiddenReplyField(false);
+      }}
+      >
+        Ответить
+      </button>
+      {!hiddenReplyField
+      && (
+      <div>
+        reply field
+        <button onClick={() => {
+          setHiddenReplyField(true);
+        }}
+        >
+          Отменить
+        </button>
+      </div>
+      )}
     </div>
   );
 };
