5697f0a3e1f6d7505e6aa9c31fdbb55c569a461e
Max <armageddon4839@gmail.com>
2020-07-27 15:08:18 +0300

    now connects to socket only when clicking play button, fixed player loading and play buttons

--- frontend/src/components/Video.jsx
+++ frontend/src/components/Video.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { cn } from '@bem-react/classname';
 import io from 'socket.io-client';
 import './Video.css';
@@ -9,6 +9,7 @@ const Video = () => {
   const [showVideo, setShowVideo] = useState(false);
   const [showPlay, setShowPlay] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
+  const [sendPlay, setSendPlay] = useState(false);
   const playerCss = cn('Player');
   const streaming = (stream) => {
     const path = `http://${document.location.host}/api/video/${stream.path.substring(stream.path.indexOf('video/') + 'video/'.length)}`;
@@ -18,45 +19,41 @@ const Video = () => {
     hls.attachMedia(video);
     hls.on(Hls.Events.ERROR, (event, data) => console.log(event, data));
   };
-
-  const socket = io(`${document.location.hostname}:8000`, { query: { movie: window.location.pathname, torrentFile: 'public/torrent-files/forrest-gump.torrent' } });
-  console.log(socket);
-  socket.on('stream', (stream) => {
-    streaming(stream);
-    socket.off('stream');
-  });
+  const playVideo = () => {
+    const socket = io(`${document.location.hostname}:8000`, { query: { movie: window.location.pathname, torrentFile: 'public/torrent-files/forrest-gump.torrent' } });
+    if (!sendPlay) socket.emit('play');
+    setSendPlay(true);
+    setShowPlay(false);
+    setIsLoading(true);
+    socket.on('stream', (stream) => {
+      streaming(stream);
+      socket.off('stream');
+    });
+  };
   return (
-    <div
-      tabIndex={0}
-      role="button"
-      id="container"
-      className={playerCss()}
-      onClick={() => {
-        socket.emit('play');
-      }}
-    >
-      {showPlay && (
-        <div
-          onClick={() => {
-            setShowPlay(false);
-            setIsLoading(true);
-          }}
-          className={playerCss('PlayBox')}
-        >
+      <div
+          tabIndex={0}
+          role="button"
+          id="container"
+          className={playerCss()}
+          onClick={() => playVideo()}
+      >
+        {showPlay && (
+            <div className={playerCss('PlayBox')}>
           <span className={playerCss('PlayIcon', ['material-icons'])}>
             play_circle_outline
           </span>
-        </div>
-      )}
-      {isLoading && !showVideo && (
-        <div className={playerCss('PlayBox')}>
-          <Loader type="Circles" color="#551A8B" />
-        </div>
-      )}
-      {showVideo && (
-        <video id="video" className={playerCss('Movie')} controls width="640" height="360" />
-      )}
-    </div>
+            </div>
+        )}
+        {isLoading && !showVideo && (
+            <div className={playerCss('PlayBox')}>
+              <Loader type="Circles" color="#551A8B" />
+            </div>
+        )}
+        {showVideo && (
+            <video id="video" className={playerCss('Movie')} controls width="640" height="360" />
+        )}
+      </div>
   );
 };
 
