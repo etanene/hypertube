861f870742d06ecfcb4ad2a1f2a220a3be9d7b82
Maksix <armageddon4839@gmail.com>
2020-08-02 19:36:41 +0300

    changed root user

--- backend/src/db/migrates/root.sql
+++ backend/src/db/migrates/root.sql
@@ -1,6 +1,6 @@
 INSERT INTO
-  users (login, email, first_name, last_name, validate, photo, passwd)
+  users (login, email, first_name, last_name, passwd, photo, validate)
 VALUES
-  ('root', 'kratyuk@mail.ru', 'root', 'root', TRUE, 'avatar.jpg', '$2b$04$tQciV8PEeCfqEZdEMtN4kO8LPDPx3WOAIIMVC2qtkIN6SZ/fxFokG')
+  ('root', 'kratyuk@mail.ru', 'root', 'root', '$2b$04$tQciV8PEeCfqEZdEMtN4kO8LPDPx3WOAIIMVC2qtkIN6SZ/fxFokG', 'avatar.jpg', TRUE)
 ON CONFLICT (login)
   DO NOTHING;
